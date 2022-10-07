19de6a798ab0b871ab24c971337212f083d3735e
Joe <105400893+joesen-dev@users.noreply.github.com>
2022-10-07 18:31:29 -0400

    [ISSUE #15] - Resume styling  - Updated resume styling with Bootstrap

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -10,30 +10,32 @@ function Resume() {
       <span>
         Download my{" "}
         <a href={resume} download>
-          <button>resume</button>
+          <button type="button" className="btn btn-success">
+            resume
+          </button>
         </a>
       </span>
       <div>
         <h3>Front-end Proficiencies</h3>
-        <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>JavaScript</li>
-          <li>responsive design</li>
-          <li>React</li>
-          <li>Bootstrap</li>
+        <ul className="list-group">
+          <li className="list-group-item">HTML</li>
+          <li className="list-group-item">CSS</li>
+          <li className="list-group-item">JavaScript</li>
+          <li className="list-group-item">responsive design</li>
+          <li className="list-group-item">React</li>
+          <li className="list-group-item">Bootstrap</li>
         </ul>
       </div>
       <div>
         <h3>Back-end Proficiencies</h3>
-        <ul>
-          <li>APIs</li>
-          <li>Node</li>
-          <li>Express</li>
-          <li>MySQL, Sequelize</li>
-          <li>MongoDB, Mongoose</li>
-          <li>REST</li>
-          <li>GraphQL</li>
+        <ul className="list-group">
+          <li className="list-group-item">APIs</li>
+          <li className="list-group-item">Node</li>
+          <li className="list-group-item">Express</li>
+          <li className="list-group-item">MySQL, Sequelize</li>
+          <li className="list-group-item">MongoDB, Mongoose</li>
+          <li className="list-group-item">REST</li>
+          <li className="list-group-item">GraphQL</li>
         </ul>
       </div>
     </section>
