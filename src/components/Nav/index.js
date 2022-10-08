700229f830b784eab5962721ce4dd92be2b04ff1
Joe <105400893+joesen-dev@users.noreply.github.com>
2022-10-08 19:03:44 -0400

    [ISSUE #15]  - edits to nav and index.css

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -10,63 +10,81 @@ function Nav(props) {
   return (
     <section className="hero">
       <header className="App-header">
-        <nav className="navbar text-white">
-          <div className="container-fluid col-sm-6 justify-content-start">
-            <img
-              src={avatar}
-              className="avatar"
-              alt="Joseph's portfolio avatar"
-            />
+        <nav className="navbar navbar-expand-lg">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#about">
+              <img
+                src={avatar}
+                className="avatar"
+                alt="Joseph's portfolio avatar"
+              />
+            </a>
+            <button
+              className="navbar-toggler navbar-light"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div
+              className="collapse navbar-collapse justify-content-end"
+              id="navbarNav">
+              <ul className="navbar-nav nav-pills justify-content-end">
+                <li className="nav-item">
+                  <a
+                    href="#about"
+                    onClick={() => handlePageChange("About")}
+                    // (ternary) operator that checks to see if the current page is "About"
+                    // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
+                    // className={currentPage === "About" ? "navActive" : "nav-link"}>
+                    className={
+                      currentPage === "About" ? "nav-link active" : "nav-link"
+                    }
+                    aria-current={currentPage === "About" ? "page" : ""}>
+                    About
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a
+                    href="#portfolio"
+                    onClick={() => handlePageChange("Portfolio")}
+                    className={
+                      currentPage === "Portfolio"
+                        ? "nav-link active"
+                        : "nav-link"
+                    }
+                    aria-current={currentPage === "Portfolio" ? "page" : ""}>
+                    Portfolio
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a
+                    href="#contact"
+                    onClick={() => handlePageChange("Contact")}
+                    className={
+                      currentPage === "Contact" ? "nav-link active" : "nav-link"
+                    }
+                    aria-current={currentPage === "Contact" ? "page" : ""}>
+                    Contact
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a
+                    href="#resume"
+                    onClick={() => handlePageChange("Resume")}
+                    className={
+                      currentPage === "Resume" ? "nav-link active" : "nav-link"
+                    }
+                    aria-current={currentPage === "Resume" ? "page" : ""}>
+                    Resume
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
-          <ul id="nav-list" className="nav nav-pills justify-content-end">
-            {/* <li><a href="#about">About me</a></li> */}
-            <li className="nav-item">
-              <a
-                href="#about"
-                onClick={() => handlePageChange("About")}
-                // (ternary) operator that checks to see if the current page is "About"
-                // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
-                // className={currentPage === "About" ? "navActive" : "nav-link"}>
-                className={
-                  currentPage === "About" ? "nav-link active" : "nav-link"
-                }>
-                About
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                href="#portfolio"
-                onClick={() => handlePageChange("Portfolio")}
-                className={
-                  currentPage === "Portfolio" ? "nav-link active" : "nav-link"
-                }>
-                Portfolio
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                href="#contact"
-                onClick={() => handlePageChange("Contact")}
-                className={
-                  currentPage === "Contact" ? "nav-link active" : "nav-link"
-                }>
-                Contact
-              </a>
-            </li>
-            <li className="nav-item">
-              <a
-                href="#resume"
-                onClick={() => handlePageChange("Resume")}
-                className={
-                  currentPage === "Resume" ? "nav-link active" : "nav-link"
-                }>
-                Resume
-              </a>
-            </li>
-          </ul>
-          <button className="hamburger" id="hamburger">
-            <i className="fas fa-bars"></i>
-          </button>
         </nav>
         <div className="hero-container">
           <div className="hero-text">
