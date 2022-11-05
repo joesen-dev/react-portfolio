f7c57bf3b05932a8c6bbc60df97a35c36d011171
Joe <105400893+joesen-dev@users.noreply.github.com>
2022-11-05 09:46:57 -0400

    Added resume and LinkedIn. Prettier ran a format

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,5 +1,5 @@
-import React from "react";
-import resume from "../../assets/images/project-images/resume-template.pdf";
+import React from 'react';
+import resume from '../../assets/Resume-Joseph-Senyonga.pdf';
 
 function Resume() {
   return (
@@ -8,34 +8,34 @@ function Resume() {
         <h2>Resume</h2>
       </div>
       <span>
-        Download my{" "}
+        Download my{' '}
         <a href={resume} download>
-          <button type="button" className="btn btn-success">
+          <button type='button' className='btn btn-success'>
             resume
           </button>
         </a>
       </span>
       <div>
         <h3>Front-end Proficiencies</h3>
-        <ul className="list-group">
-          <li className="list-group-item">HTML</li>
-          <li className="list-group-item">CSS</li>
-          <li className="list-group-item">JavaScript</li>
-          <li className="list-group-item">responsive design</li>
-          <li className="list-group-item">React</li>
-          <li className="list-group-item">Bootstrap</li>
+        <ul className='list-group'>
+          <li className='list-group-item'>HTML</li>
+          <li className='list-group-item'>CSS</li>
+          <li className='list-group-item'>JavaScript</li>
+          <li className='list-group-item'>responsive design</li>
+          <li className='list-group-item'>React</li>
+          <li className='list-group-item'>Bootstrap</li>
         </ul>
       </div>
       <div>
         <h3>Back-end Proficiencies</h3>
-        <ul className="list-group">
-          <li className="list-group-item">APIs</li>
-          <li className="list-group-item">Node</li>
-          <li className="list-group-item">Express</li>
-          <li className="list-group-item">MySQL, Sequelize</li>
-          <li className="list-group-item">MongoDB, Mongoose</li>
-          <li className="list-group-item">REST</li>
-          <li className="list-group-item">GraphQL</li>
+        <ul className='list-group'>
+          <li className='list-group-item'>APIs</li>
+          <li className='list-group-item'>Node</li>
+          <li className='list-group-item'>Express</li>
+          <li className='list-group-item'>MySQL, Sequelize</li>
+          <li className='list-group-item'>MongoDB, Mongoose</li>
+          <li className='list-group-item'>REST</li>
+          <li className='list-group-item'>GraphQL</li>
         </ul>
       </div>
     </section>
