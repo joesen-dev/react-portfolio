a2629204df4a0e76cdd7d0d8009596a27a525b9d
Joe <105400893+joesen-dev@users.noreply.github.com>
2022-11-09 18:47:45 -0500

    Nav is now responsive

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,13 +1,19 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import { BsFillMoonStarsFill } from 'react-icons/bs';
 import { DarkModeContext } from '../../utils/DarkModeContext';
 
 function Navigation(props) {
   const { currentPage, handlePageChange } = props;
   const { darkMode, toggleDarkMode } = useContext(DarkModeContext);
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  // Dark mode toggle
   const handleClick = () => {
     toggleDarkMode();
   };
+
+  // Hamburger toggle
+
   useEffect(() => {
     document.title = `${currentPage}`;
   }, [currentPage]);
@@ -17,7 +23,73 @@ function Navigation(props) {
       <h1 className='text-xl font-burtons dark:text-white'>
         <a href='/'>Portfolio</a>
       </h1>
-      <ul className='flex items-center'>
+
+      {/* Mobile Nav Menu */}
+      <section className='MOBILE-MENU flex lg:hidden'>
+        <div
+          className='HAMBURGER-ICON space-y-2'
+          // toggle isNavOpen state on click
+          onClick={() => setIsNavOpen(prev => !prev)}>
+          <span className='block h-0.5 w-8 animate-pulse bg-gray-600'></span>
+          <span className='block h-0.5 w-8 animate-pulse bg-gray-600'></span>
+          <span className='block h-0.5 w-8 animate-pulse bg-gray-600'></span>
+        </div>
+
+        <div
+          // toggle class based on isNavOpen state
+          className={isNavOpen ? 'showMenuNav' : 'hideMenuNav'}>
+          <div
+            className='CROSS-ICON absolute top-0 right-0 px-8 py-8'
+            // change isNavOpen state to false to close the menu
+            onClick={() => setIsNavOpen(false)}>
+            <svg
+              className='h-8 w-8 text-gray-600'
+              viewBox='0 0 24 24'
+              fill='none'
+              stroke='currentColor'
+              strokeWidth='2'
+              strokeLinecap='round'
+              strokeLinejoin='round'>
+              <line x1='18' y1='6' x2='6' y2='18' />
+              <line x1='6' y1='6' x2='18' y2='18' />
+            </svg>
+          </div>
+          <ul className='MENU-LINK-MOBILE-OPEN flex flex-col items-center justify-between min-h-[250px]'>
+            <li className='my-8'>
+              <BsFillMoonStarsFill
+                onClick={handleClick}
+                className='cursor-pointer text-2xl max-w-lg mx-auto dark:text-white'
+              />
+            </li>
+            <li className='my-8'>
+              <a
+                className='bg-gradient-to-r from-red-200 to-red-400 text-white px-4 py-2 rounded-md'
+                href='#about'
+                onClick={() => handlePageChange('About')}>
+                About
+              </a>
+            </li>
+            <li className='my-8'>
+              <a
+                className='bg-gradient-to-r from-red-200 to-red-400 text-white px-4 py-2 rounded-md'
+                href='#portfolio'
+                onClick={() => handlePageChange('Portfolio')}>
+                Portfolio
+              </a>
+            </li>
+            <li className='my-8'>
+              <a
+                className='bg-gradient-to-r from-red-200 to-red-400 text-white px-4 py-2 rounded-md'
+                href='#resume'
+                onClick={() => handlePageChange('Resume')}>
+                Resume
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <ul className='DESKTOP-MENU hidden space-x-8 lg:flex items-center'>
         <li>
           <BsFillMoonStarsFill
             onClick={handleClick}
