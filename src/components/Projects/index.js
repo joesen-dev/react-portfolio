a7768506e46ec330caf18b4bab3170a2b97feadd
Joe <105400893+joesen-dev@users.noreply.github.com>
2022-11-10 13:52:42 -0500

    Added project description and a link to the repo on all project cards

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -26,7 +26,7 @@ function Projects() {
             alt=''
             src={Capstone}
           />
-          <div className='grid grid-cols-1 bg-gradient-to-b from-red-400 opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 flex justify-center md:text-2xl text-sm pt-8 pb-2 px-8 text-white font-semibold'>
+          <div className='grid grid-cols-1 bg-gradient-to-b from-red-400 opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 items-center md:text-xl text-sm pt-8 pb-2 px-8 text-white font-semibold'>
             <p>
               This daily workout tracker will help you maintain your physical
               health by adding workouts to your calendar and tracking your
@@ -39,7 +39,7 @@ function Projects() {
             </div>
           </div>
         </div>
-        <div className='basis-1/3 flex-1'>
+        <div className='basis-1/3 flex-1 relative'>
           <img
             className='rounded-lg object-cover'
             width={'100%'}
@@ -48,8 +48,20 @@ function Projects() {
             alt=''
             src={Git_Social}
           />
+          <div className='grid grid-cols-1 bg-gradient-to-b from-red-400 opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 items-center md:text-xl text-sm pt-8 pb-2 px-8 text-white font-semibold'>
+            <p>
+              Git Social is a social app for new developers to post about their
+              projects and solicit help from other developers looking to build
+              their portfolios.
+            </p>
+            <div className='text-5xl flex justify-center gap-16 py-3 text-white dark:text-gray-400'>
+              <a href='https://github.com/joesen-dev/git-social'>
+                <AiFillGithub />
+              </a>
+            </div>
+          </div>
         </div>
-        <div className='basis-1/3 flex-1'>
+        <div className='basis-1/3 flex-1 relative'>
           <img
             className='rounded-lg object-cover'
             width={'100%'}
@@ -58,8 +70,19 @@ function Projects() {
             alt=''
             src={FOODr}
           />
+          <div className='grid grid-cols-1 bg-gradient-to-b from-red-400 opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 items-center md:text-xl text-sm pt-8 pb-2 px-8 text-white font-semibold'>
+            <p>
+              FOODr is a recommendation app that suggests local restaurants near
+              the user.
+            </p>
+            <div className='text-5xl flex justify-center gap-16 py-3 text-white dark:text-gray-400'>
+              <a href='https://github.com/joesen-dev/projectAwesome'>
+                <AiFillGithub />
+              </a>
+            </div>
+          </div>
         </div>
-        <div className='basis-1/3 flex-1'>
+        <div className='basis-1/3 flex-1 relative'>
           <img
             className='rounded-lg object-cover'
             width={'100%'}
@@ -68,8 +91,19 @@ function Projects() {
             alt=''
             src={README_Pro}
           />
+          <div className='grid grid-cols-1 bg-gradient-to-b from-red-400 opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 items-center md:text-xl text-sm pt-8 pb-2 px-8 text-white font-semibold'>
+            <p>
+              The README generator is designed to streamline creating
+              Professional README files for developers
+            </p>
+            <div className='text-5xl flex justify-center gap-16 py-3 text-white dark:text-gray-400'>
+              <a href='https://github.com/joesen-dev/readme-pro'>
+                <AiFillGithub />
+              </a>
+            </div>
+          </div>
         </div>
-        <div className='basis-1/3 flex-1'>
+        <div className='basis-1/3 flex-1 relative'>
           <img
             className='rounded-lg object-cover'
             width={'100%'}
@@ -78,8 +112,19 @@ function Projects() {
             alt=''
             src={Staff_Tracker}
           />
+          <div className='grid grid-cols-1 bg-gradient-to-b from-red-400 opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 items-center md:text-xl text-sm pt-8 pb-2 px-8 text-white font-semibold'>
+            <p>
+              View and manage the the departments, roles, and employees in your
+              company in the command line with Staff Tracker.
+            </p>
+            <div className='text-5xl flex justify-center gap-16 py-3 text-white dark:text-gray-400'>
+              <a href='https://github.com/joesen-dev/staff-tracker'>
+                <AiFillGithub />
+              </a>
+            </div>
+          </div>
         </div>
-        <div className='basis-1/3 flex-1'>
+        <div className='basis-1/3 flex-1 relative'>
           <img
             className='rounded-lg object-cover'
             width={'100%'}
@@ -88,6 +133,17 @@ function Projects() {
             alt=''
             src={Team_Builder}
           />
+          <div className='grid grid-cols-1 bg-gradient-to-b from-red-400 opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 items-center md:text-xl text-sm pt-8 pb-2 px-8 text-white font-semibold'>
+            <p>
+              A command line application that takes user input and builds a
+              webpage with the team's basic information.
+            </p>
+            <div className='text-5xl flex justify-center gap-16 py-3 text-white dark:text-gray-400'>
+              <a href='https://github.com/joesen-dev/team-builder'>
+                <AiFillGithub />
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </section>
