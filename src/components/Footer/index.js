3177c316689df8408a16d95ce8620f415de40825
Joe <105400893+joesen-dev@users.noreply.github.com>
2022-11-16 12:58:22 -0500

    Update to resume and footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer className='p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800'>
       <span className='text-sm text-gray-500 sm:text-center dark:text-gray-400'>
-        by Joseph Senyonga 2022{' '}
+        Email: senjoseph@gmail.com{' '}
       </span>
       <div className='flex mt-4 space-x-6 sm:justify-center sm:mt-0'>
         <a
