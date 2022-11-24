49e0237137895e61aee380e0d777c76f66b51b10
Joe <105400893+joesen-dev@users.noreply.github.com>
2022-11-24 15:22:11 -0500

    Updated project cards and images

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,19 +4,19 @@ import { AiFillLinkedin, AiFillGithub } from 'react-icons/ai';
 function Footer() {
   return (
     <footer className='p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800'>
-      <span className='text-sm text-gray-500 sm:text-center dark:text-gray-400'>
+      <span className='text-sm text-gray-500 sm:text-center dark:text-white'>
         Email: josephsenyonga38@gmail.com{' '}
       </span>
       <div className='flex mt-4 space-x-6 sm:justify-center sm:mt-0'>
         <a
           href='https://www.linkedin.com/in/josephsenyonga38/'
-          className='text-gray-600 hover:text-gray-900 dark:hover:text-white'>
+          className='text-gray-500 hover:text-gray-900 dark:hover:text-white'>
           <AiFillLinkedin />
           <span className='sr-only'>LinkedIn page</span>
         </a>
         <a
           href='https://github.com/joesen-dev'
-          className='text-gray-600 hover:text-gray-900 dark:hover:text-white'>
+          className='text-gray-500 hover:text-gray-900 dark:hover:text-white'>
           <AiFillGithub />
           <span className='sr-only'>GitHub account</span>
         </a>
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { AiFillGithub } from 'react-icons/ai';
 import Capstone from '../../assets/images/project-images/Capstone.png';
-import Git_Social from '../../assets/images/project-images/Git_social.png';
-import FOODr from '../../assets/images/project-images/FOODr.png';
+import Git_Social from '../../assets/images/project-images/Git_Social.png';
+import Note_Taker from '../../assets/images/project-images/Notes-page.png';
 import README_Pro from '../../assets/images/project-images/README_Pro.png';
 import Team_Builder from '../../assets/images/project-images/Team_Builder.png';
 import Staff_Tracker from '../../assets/images/project-images/Staff_Tracker.png';
 
 function Projects() {
   return (
-    <section section id='projects'>
+    <section id='projects'>
       <div>
         <h3 className='text-3xl py-1 dark:text-white'>Projects</h3>
         <p className='text-md py-2 leading-8 text-gray-800 dark:text-gray-200'>
@@ -17,131 +17,263 @@ function Projects() {
         </p>
       </div>
       <div className='flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap'>
-        <div className=' basis-1/3 flex-1 relative'>
+        {/* Capstone */}
+        <div className='basis-1/3 flex-1 relative bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700'>
           <img
-            className='rounded-lg object-cover'
+            className='rounded-t-lg'
             width={'100%'}
             height={'100%'}
             layout='responsive'
-            alt=''
             src={Capstone}
+            alt='Capstone langing page'
           />
-          <div className='grid grid-cols-1 bg-gradient-to-b from-red-400 opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 items-center md:text-xl text-sm pt-8 pb-2 px-8 text-white font-semibold'>
-            <p>
+          <div className='p-5'>
+            <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
+              Capstone
+            </h5>
+            <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>
               This daily workout tracker will help you maintain your physical
               health by adding workouts to your dashboard and tracking your
               progress over the months.
             </p>
-            <div className='text-5xl flex justify-center gap-16 py-3 text-white '>
-              <a href='https://github.com/leunggerry/personal-health-tracker'>
+            <div className='w-full inline-flex justify-between'>
+              <a
+                href='https://whispering-hamlet-84520.herokuapp.com/'
+                className='inline-flex items-center font-medium text-blue-600 hover:text-blue-800 dark:text-blue-500 dark:hover:text-blue-700'>
+                Live Link
+                <svg
+                  className='w-6 h-6 ml-1'
+                  fill='currentColor'
+                  viewBox='0 0 20 20'
+                  xmlns='http://www.w3.org/2000/svg'>
+                  <path
+                    fillRule='evenodd'
+                    d='M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z'
+                    clipRule='evenodd'></path>
+                </svg>
+              </a>
+              <a
+                href='https://github.com/leunggerry/personal-health-tracker'
+                className='text-gray-500 text-2xl hover:text-gray-900 dark:hover:text-white'>
                 <AiFillGithub />
+                <span className='sr-only'>GitHub Repository</span>
               </a>
             </div>
           </div>
         </div>
-        <div className='basis-1/3 flex-1 relative'>
+        {/* Git Social */}
+        <div className='basis-1/3 flex-1 relative bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700'>
           <img
-            className='rounded-lg object-cover'
+            className='rounded-t-lg'
             width={'100%'}
             height={'100%'}
             layout='responsive'
-            alt=''
             src={Git_Social}
+            alt='Git Social Landing Page'
           />
-          <div className='grid grid-cols-1 bg-gradient-to-b from-red-400 opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 items-center md:text-xl text-sm pt-8 pb-2 px-8 text-white font-semibold'>
-            <p>
+          <div className='p-5'>
+            <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
+              Git Social
+            </h5>
+            <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>
               Git Social is a social app for new developers to post about their
               projects and solicit help from other developers looking to build
               their portfolios.
             </p>
-            <div className='text-5xl flex justify-center gap-16 py-3 text-white '>
-              <a href='https://github.com/joesen-dev/git-social'>
+            <div className='w-full inline-flex justify-between'>
+              <a
+                href='https://pacific-taiga-17313.herokuapp.com/'
+                className='inline-flex items-center font-medium text-blue-600 hover:text-blue-800 dark:text-blue-500 dark:hover:text-blue-700'>
+                Live Link
+                <svg
+                  className='w-6 h-6 ml-1'
+                  fill='currentColor'
+                  viewBox='0 0 20 20'
+                  xmlns='http://www.w3.org/2000/svg'>
+                  <path
+                    fillRule='evenodd'
+                    d='M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z'
+                    clipRule='evenodd'></path>
+                </svg>
+              </a>
+              <a
+                href='https://github.com/leunggerry/git-social'
+                className='text-gray-500 text-2xl hover:text-gray-900 dark:hover:text-white'>
                 <AiFillGithub />
+                <span className='sr-only'>GitHub Repository</span>
               </a>
             </div>
           </div>
         </div>
-        <div className='basis-1/3 flex-1 relative'>
+        {/* Team Builder */}
+        <div className='basis-1/3 flex-1 relative bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700'>
           <img
-            className='rounded-lg object-cover'
+            className='rounded-t-lg'
             width={'100%'}
             height={'100%'}
             layout='responsive'
-            alt=''
-            src={FOODr}
+            src={Team_Builder}
+            alt='Team Builder Page'
           />
-          <div className='grid grid-cols-1 bg-gradient-to-b from-red-400 opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 items-center md:text-xl text-sm pt-8 pb-2 px-8 text-white font-semibold'>
-            <p>
-              FOODr is a recommendation app that suggests local restaurants near
-              the user.
+          <div className='p-5'>
+            <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
+              Team Builder
+            </h5>
+            <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>
+              Team builder is a terminal-based application designed for managers
+              that takes user input and generates a basic web page with their
+              team.
             </p>
-            <div className='text-5xl flex justify-center gap-16 py-3 text-white '>
-              <a href='https://github.com/joesen-dev/projectAwesome'>
+            <div className='w-full inline-flex justify-between'>
+              <a
+                href='https://github.com/joesen-dev/team-builder'
+                className='inline-flex items-center font-medium text-blue-600 hover:text-blue-800 dark:text-blue-500 dark:hover:text-blue-700'>
+                Source code
+                <svg
+                  className='w-6 h-6 ml-1'
+                  fill='currentColor'
+                  viewBox='0 0 20 20'
+                  xmlns='http://www.w3.org/2000/svg'>
+                  <path
+                    fillRule='evenodd'
+                    d='M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z'
+                    clipRule='evenodd'></path>
+                </svg>
+              </a>
+              <a
+                href='https://github.com/joesen-dev/team-builder'
+                className='text-gray-500 text-2xl hover:text-gray-900 dark:hover:text-white'>
                 <AiFillGithub />
+                <span className='sr-only'>GitHub Repository</span>
               </a>
             </div>
           </div>
         </div>
-        <div className='basis-1/3 flex-1 relative'>
-          {/* TODO: get an updated screenshot */}
+        {/* README Pro*/}
+        <div className='basis-1/3 flex-1 relative bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700'>
           <img
-            className='rounded-lg object-cover'
+            className='rounded-t-lg'
             width={'100%'}
             height={'100%'}
             layout='responsive'
-            alt=''
             src={README_Pro}
+            alt='Team Builder Page'
           />
-          <div className='grid grid-cols-1 bg-gradient-to-b from-red-400 opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 md:text-xl text-sm pt-2 pb-2 px-8 text-white font-semibold'>
-            <p>
+          <div className='p-5'>
+            <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
+              README Pro
+            </h5>
+            <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>
               The README generator is designed to streamline creating
               Professional README files for developers
             </p>
-            <div className='text-5xl flex justify-center gap-16 py-2 text-white '>
-              <a href='https://github.com/joesen-dev/readme-pro'>
+            <div className='w-full inline-flex justify-between'>
+              <a
+                href='https://github.com/joesen-dev/readme-pro'
+                className='inline-flex items-center font-medium text-blue-600 hover:text-blue-800 dark:text-blue-500 dark:hover:text-blue-700'>
+                Source code
+                <svg
+                  className='w-6 h-6 ml-1'
+                  fill='currentColor'
+                  viewBox='0 0 20 20'
+                  xmlns='http://www.w3.org/2000/svg'>
+                  <path
+                    fillRule='evenodd'
+                    d='M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z'
+                    clipRule='evenodd'></path>
+                </svg>
+              </a>
+              <a
+                href='https://github.com/joesen-dev/readme-pro'
+                className='text-gray-500 text-2xl hover:text-gray-900 dark:hover:text-white'>
                 <AiFillGithub />
+                <span className='sr-only'>GitHub Repository</span>
               </a>
             </div>
           </div>
         </div>
-        <div className='basis-1/3 flex-1 relative'>
+        {/* Staff Tracker*/}
+        <div className='basis-1/3 flex-1 relative bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700'>
           <img
-            className='rounded-lg object-cover'
+            className='rounded-t-lg'
             width={'100%'}
             height={'100%'}
             layout='responsive'
-            alt=''
             src={Staff_Tracker}
+            alt='Team Builder Page'
           />
-          <div className='grid grid-cols-1 bg-gradient-to-b from-red-400 opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 items-center md:text-xl text-sm pt-8 pb-2 px-8 text-white font-semibold'>
-            <p>
+          <div className='p-5'>
+            <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
+              Staff Tracker
+            </h5>
+            <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>
               View and manage the the departments, roles, and employees in your
               company in the command line with Staff Tracker.
             </p>
-            <div className='text-5xl flex justify-center gap-16 py-3 text-white '>
-              <a href='https://github.com/joesen-dev/staff-tracker'>
+            <div className='w-full inline-flex justify-between'>
+              <a
+                href='https://github.com/joesen-dev/staff-tracker'
+                className='inline-flex items-center font-medium text-blue-600 hover:text-blue-800 dark:text-blue-500 dark:hover:text-blue-700'>
+                Source code
+                <svg
+                  className='w-6 h-6 ml-1'
+                  fill='currentColor'
+                  viewBox='0 0 20 20'
+                  xmlns='http://www.w3.org/2000/svg'>
+                  <path
+                    fillRule='evenodd'
+                    d='M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z'
+                    clipRule='evenodd'></path>
+                </svg>
+              </a>
+              <a
+                href='https://github.com/joesen-dev/staff-tracker'
+                className='text-gray-500 text-2xl hover:text-gray-900 dark:hover:text-white'>
                 <AiFillGithub />
+                <span className='sr-only'>GitHub Repository</span>
               </a>
             </div>
           </div>
         </div>
-        <div className='basis-1/3 flex-1 relative'>
+        {/* Staff Tracker*/}
+        <div className='basis-1/3 flex-1 relative bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700'>
           <img
-            className='rounded-lg object-cover'
+            className='rounded-t-lg'
             width={'100%'}
             height={'100%'}
             layout='responsive'
-            alt=''
-            src={Team_Builder}
+            src={Note_Taker}
+            alt='Team Builder Page'
           />
-          <div className='grid grid-cols-1 bg-gradient-to-b from-red-400 opacity-0 hover:opacity-100 duration-300 absolute inset-0 z-10 items-center md:text-xl text-sm pt-8 pb-2 px-8 text-white font-semibold'>
-            <p>
-              A command line application that takes user input and builds a
-              webpage with the team's basic information.
+          <div className='p-5'>
+            <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
+              Staff Tracker
+            </h5>
+            <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>
+              View and manage the the departments, roles, and employees in your
+              company in the command line with Staff Tracker.
             </p>
-            <div className='text-5xl flex justify-center gap-16 py-3 text-white '>
-              <a href='https://github.com/joesen-dev/team-builder'>
+            <div className='w-full inline-flex justify-between'>
+              <a
+                href='https://github.com/joesen-dev/staff-tracker'
+                className='inline-flex items-center font-medium text-blue-600 hover:text-blue-800 dark:text-blue-500 dark:hover:text-blue-700'>
+                Source code
+                <svg
+                  className='w-6 h-6 ml-1'
+                  fill='currentColor'
+                  viewBox='0 0 20 20'
+                  xmlns='http://www.w3.org/2000/svg'>
+                  <path
+                    fillRule='evenodd'
+                    d='M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z'
+                    clipRule='evenodd'></path>
+                </svg>
+              </a>
+              <a
+                href='https://github.com/joesen-dev/staff-tracker'
+                className='text-gray-500 text-2xl hover:text-gray-900 dark:hover:text-white'>
                 <AiFillGithub />
+                <span className='sr-only'>GitHub Repository</span>
               </a>
             </div>
           </div>
