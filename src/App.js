18799701fc77e7dd9d9ba2faada87028426ec82f
Joe <105400893+joesen-dev@users.noreply.github.com>
2023-05-11 12:40:38 -0400

    fixing react GA initialization

--- src/App.js
+++ src/App.js
@@ -4,17 +4,15 @@ import PortfolioContainer from './components/PortfolioContainer';
 import { DarkModeProvider } from './context/DarkModeContext';
 
 // initialize Google Analytics
-const TRACKING_ID = process.env.React_App_TRACKING_ID;
+const TRACKING_ID = process.env.REACT_APP_TRACKING_ID;
 ReactGA.initialize(TRACKING_ID);
 
 const App = () => {
   return (
     <div className='App'>
-      <ReactGA.GoogleAnalyticsProvider>
-        <DarkModeProvider>
-          <PortfolioContainer />
-        </DarkModeProvider>
-      </ReactGA.GoogleAnalyticsProvider>
+      <DarkModeProvider>
+        <PortfolioContainer />
+      </DarkModeProvider>
     </div>
   );
 };
